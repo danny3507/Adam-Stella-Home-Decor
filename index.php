212e24ac60e1455bfd5cee03c6214cc01acc9fe4
danny3507 <danielcalvert13@yahoo.com>
2015-07-31 22:25:27 -0500

    implemented php templating

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include ( "header.php"); ?>
+
+<div class="secondhead">
+    <div class="container">
+        <h2>You name it; we can make it!</h2>
+    </div>
+</div>
+
+<?php include ( "footer.php"); ?>
+    
